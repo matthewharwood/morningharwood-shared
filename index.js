1a8e9a643b8caeebd6411366f8fbc89836860e48
Matthew Harwood <matthhar12@gmail.com>
2017-07-09 15:49:20 -0700

    - init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by matth on 7/8/2017.
+ */
+
+const Routes = {
+  PEOPLE: 'people',
+  WORKS: 'works',
+  END_POINTS: 'endpoints',
+  TOUTS: 'touts',
+  TOOLTIPS: 'tooltips',
+};
+
+module.exports = Routes;
